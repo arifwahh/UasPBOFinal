bd584df11dac4b3c1f543ac657b45948a1855a7d
hp <hp@DESKTOP-1L2A0O9>
2022-12-16 15:45:54 +0700

    Add project files.

---
+++ UasDong/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using UasDong.Models;
+
+namespace UasDong.DataContext
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext():base(nameOrConnectionString:"Myconnection")
+        {
+            
+        
+        }
+
+        public virtual DbSet<EmpClass> Empobj { get; set; }
+
+    }
+
+}
